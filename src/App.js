3f9841a473964d1fa0620e8aaa501a487baf22f8
Hyeok Min Kwon <khmzxc@naver.com>
2020-10-27 16:33:58 +0900

    OG tag 수정

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
 import Resume from "./components/Resume";
+import { Helmet } from "react-helmet";
 
 function App() {
 	return (
 		<div>
+			<Helmet>
+				<meta property="description" content="Blog" />
+				<meta property="og:site_name" content="Blog" />
+				<meta property="og:title" content="Blog" />
+				<meta property="og:description" content="emscb.github.io/emscb_blog" />
+			</Helmet>
 			<div
 				style={{
 					display: "flex",
